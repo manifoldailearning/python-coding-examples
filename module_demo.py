3cfd6e7f332c2726cc5086f48a23c7de60c6634a
nachiketh <support@manifoldailearning.in>
2024-02-17 22:53:14 +0530

    module_souce code

---
+++ module_demo.py
@@ -0,0 +1,15 @@
+import math
+v = 100
+# print("hello world")
+
+def something():
+    print("Something")
+
+def something_else():
+     print("Something Else")
+
+if __name__ == "__main__" :
+    something_else()
+    print("Welcome to the class")
+    print("Completed")
+    
---
+++ module_example.py
@@ -0,0 +1,8 @@
+n1 = 10
+n2 = 20
+
+def addition(num1,num2):
+    return num1+num2
+
+def multiplication(num1,num2):
+    return num1*num2

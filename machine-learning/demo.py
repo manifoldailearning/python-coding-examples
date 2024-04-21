b6d7f3f576d1dfec7ac5b4df3dd3d145fce6a9cb
Nachiketh <murthy@manifoldailearning.in>
2024-04-21 19:36:04 +0530

    regularizers

---
+++ machine-learning/demo.py
@@ -0,0 +1,13 @@
+# Build fast api application
+
+# load the model
+
+# implement post method
+
+# get the new dataset
+
+# apply model.predict()
+
+# Example - https://github.com/manifoldailearning/Complete-MLOps-BootCamp/blob/main/Build-ML-App-FASTAPI/loan_fast_api_app.py
+
+# Streamlit Example - https://github.com/manifoldailearning/Complete-MLOps-BootCamp/blob/main/Build-ML-App-Streamlit/streamlit-ml-app.py
